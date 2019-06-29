7302a56191118bae5c315b0c34d5e01559b256cf
clairemgiovanoni <43757653+clairemgiovanoni@users.noreply.github.com>
2019-06-29 12:59:34 -0500

    Checkpoint 11 complete

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,6 +1,8 @@
+
 {
   $('button#play-pause').on('click', function() {
-    player.playPause();
+//     player.playPause();
+       helper.playPauseAndUpdate();
     $(this).attr('playState', player.playState);
   });
 
@@ -12,19 +14,43 @@
     if (nextSongIndex >= album.songs.length) { return; }
 
     const nextSong = album.songs[nextSongIndex];
-    player.playPause(nextSong);
+//     player.playPause(nextSong);
+       helper.playPauseAndUpdate(nextSong);
+
+$('#time-control .total-time').text( nextSong.duration );
+
   });
 
+   $('#time-control input').on('input', function (event) {
+        player.skipTo(event.target.value);
+  });
+
+$('#volume-control input').on('input', function (event) {
+        player.setVolume(event.target.value);
+  });
 
   $('button#previous').on('click', function() {
     if (player.playState !== 'playing') { return; }
 
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-    const nextSongIndex = currentSongIndex - 1;
-    if (nextSongIndex >= album.songs.length) { return; }
+    const previousSongIndex = currentSongIndex - 1;
+    if (previousSongIndex < 0 ) { return; }
 
-    const nextSong = album.songs[nextSongIndex];
-    player.playPause(nextSong);
+    const previousSong = album.songs[previousSongIndex];
+//     player.playPause(nextSong);
+       helper.playPauseAndUpdate(previousSong);
+
+$('#time-control .total-time').text( previousSong.duration );
+
   });
+
+   setInterval( () => {
+     if (player.playState !== 'playing') { return; }
+     const currentTime = player.getTime();
+     const duration = player.getDuration();
+     const percent = (currentTime / duration) * 100;
+     $('#time-control .current-time').text( currentTime );
+	 $('#time-control input').val(percent);
+   }, 1000);
 
 }
